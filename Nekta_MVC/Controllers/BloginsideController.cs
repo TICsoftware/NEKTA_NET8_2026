901e04ad0d1eb204893fae994d3983db455b70ae
Shubham Tayde <shubham.tayde@ticworks.com>
2026-07-24 11:50:53 +0530

    blog inside

---
+++ Nekta_MVC/Controllers/BloginsideController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Nekta_MVC.Models;
+
+namespace Nekta_MVC.Controllers;
+
+public class BloginsideController : Controller
+{
+    private readonly ILogger<BloginsideController> _logger;
+
+    public BloginsideController(ILogger<BloginsideController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
